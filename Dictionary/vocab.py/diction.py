718c4e9783e03f938148ef8c0a202d0de957627e
Ryan567dot <itsbluey1010@gmail.com>
2024-09-25 22:15:23 +0600

    Dictionary
    Dictionary

---
+++ Dictionary/vocab.py/diction.py
@@ -0,0 +1,27 @@
+#Dictionary
+
+Ryan ={
+    "age" : 13,
+    "School" : "ABC school",
+    "City" :"Dhaka",
+    "Country" :"Bangladesh",
+    "Maths" :"94",
+    
+}
+
+Indian ={
+    "N_fruit" : "Mango"
+    "N_bird" ; "Peacock"
+    
+}
+
+Bangladesh ={
+    "N_fruit" : "Jackfruit",
+    "N_bird" : "Magpie-robin",
+    "N_river" : "Padma river",
+    
+}
+
+#Accessing an element
+
+print(Ryan["Maths"])
